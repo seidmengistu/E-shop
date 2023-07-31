6178ddeb346079c4f54d11eb276d0657cc550634
seidmengistu <seidmengistu88@gmail.com>
2023-07-31 14:34:22 +0300

    Backend setup Finished

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,52 @@
 /*
-The below server-side JavaScript file for a React application using the Express.js framework. Let's break down what this code does:
+The below code provided is a Node.js application that uses the Express framework to create a web server. Let's go through the code step by step to understand what it does:
 
-Importing required modules:
+1:Import required modules:
 
-1.The code begins by importing the "express" module, which is a popular web application framework for Node.js used to build server-side applications.
-Creating an Express application instance:
+express: The Express framework to create the web server and handle HTTP requests.
+ErrorHandler: A custom error handler utility to handle errors in the application.
+cookieParser: Middleware to parse and handle cookies in incoming HTTP requests.
+bodyParser: Middleware to parse and handle the request body (usually sent as JSON or form data).
+fileUpload: Middleware to handle file uploads in HTTP requests.
 
-2.The code creates an instance of the Express application by calling express() and assigns it to the variable app.
-Configuration for non-production environments:
+2.Create an Express app instance:
 
-3.The code checks the value of the environment variable NODE_ENV to determine if the application is running in production mode or not.
-If the environment is not set to "PRODUCTION," the code loads and configures environment variables from a .env file located in the "backend/config/" directory.
-The dotenv module is used to load environment variables from the .env file and make them available in the Node.js process.
+const app = express();
 
-4.Exporting the Express application:
+3.Set up middleware:
 
-The app instance, which represents the Express application, is exported using module.exports, making it available for use in other parts of the application.
-In summary, this code sets up an Express.js application and loads environment variables from a .env file if the application is not running in production mode.
- The Express application instance is then exported for use in other parts of the application, where it can be used to define routes, middleware, and other functionalities required for building the server-side of a React application.
-*/
+express.json(): Middleware to parse JSON data in the request body.
+cookieParser(): Middleware to handle cookies in the request headers.
+bodyParser.urlencoded({ extended: true }): Middleware to parse form data (if any) in the request body.
+fileUpload({ useTemplateFiles: true }): Middleware to handle file uploads, allowing the use of HTML <input type="file"> elements in forms.
+
+4.Environment Configuration:
+
+The code checks if the NODE_ENV environment variable is not set to "PRODUCTION." If it's not in production mode, it loads the environment variables from a file named .env located in the backend/config directory using the dotenv package. 
+The dotenv package loads the variables from the file and makes them available in the application via process.env.
+
+5.Error Handling:
+
+The custom ErrorHandler middleware is used for error handling. This means that if there is an error in any of the routes or middlewares that follow, the error will be passed to the ErrorHandler middleware for centralized error handling.
+Export the app:
+
+The app instance is exported to be used in other parts of the application or other files.
+In summary, this code sets up an Express application, configures various middlewares for handling request data, and sets up environment variables for non-production environments.
+It also implements centralized error handling with a custom ErrorHandler middleware.
+
+ */
 
 const express=require("express");
 const ErrorHandler = require("./utils/ErrorHandler");
 const app=express();
+const cookieParser=require("cookie-parser");
+const bodyParser=require("body-parser");
+const fileUpload=require("express-fileupload");
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(fileUpload({useTemplateFiles:true}));
 
 //config
 
