30411708f497c24fe82604d6f84550af81fc7b10
seidmengistu <seidmengistu88@gmail.com>
2023-08-01 00:05:16 +0300

    Adding gitignore

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -4,6 +4,7 @@ const User=require('../model/user');
 const router=express.Router();
 const {upload}=require('../multer');
 const ErrorHandler = require('../utils/ErrorHandler');
+const fs=require("fs");
 
 
 router.post("/create-user",upload.single("file"),async(req, res,next) =>{
@@ -11,10 +12,20 @@ router.post("/create-user",upload.single("file"),async(req, res,next) =>{
                 const {name,email,password}=req.body;
 
             const userEmail=await User.findOne({email});
-            console.log(userEmail);
+            
            
 
             if(userEmail){
+                const filename=req.file.filename;
+                const filepath=`uploads/${filename}`;
+               fs.unlink(filepath,(err)=>{
+                   if(err) {
+                       console.log(err);
+                       res.status(500).json({message:"Error deleting file"});
+                   }else{
+                       res.json({message:"File deleted successfully"});
+                   }
+               })
                 return next(new ErrorHandler("User already exists",400));
             }
             const filename=req.file.filename;
--- backend/model/user.js
+++ backend/model/user.js
@@ -47,16 +47,9 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: "user",
   },
-//   avatar:{
-//     public_id: {
-//       type: String,
-//       required: true,
-//     },
-//     url: {
-//       type: String,
-//       required: true,
-//     },
-//  },
+  avatar:{
+    
+ },
  createdAt:{
   type: Date,
   default: Date.now(),
